00d50b8f044f8071befa38ccf44343749d4e3bad
gatrot <rotmangat@gmail.com>
2024-03-06 10:56:47 +0200

    Iinitial commit

---
+++ src/test/java/com/rentals/app/RentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentals.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
