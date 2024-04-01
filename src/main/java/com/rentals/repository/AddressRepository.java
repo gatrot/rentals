32cbfd10aa353a86f7e1fdb09fda27d791ab5360
gatrot <rotmangat@gmail.com>
2024-04-01 10:58:32 +0300

    deleted unused imported packages

--- src/main/java/com/rentals/repository/AddressRepository.java
+++ src/main/java/com/rentals/repository/AddressRepository.java
@@ -3,10 +3,7 @@ package com.rentals.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.rentals.entity.Address;
 
