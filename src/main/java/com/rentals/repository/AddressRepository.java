4c48fa6c6deeec098aefb9b578f291f7ca8da40e
gatrot <rotmangat@gmail.com>
2024-04-01 10:59:05 +0300

    beautify

--- src/main/java/com/rentals/repository/AddressRepository.java
+++ src/main/java/com/rentals/repository/AddressRepository.java
@@ -3,11 +3,12 @@ package com.rentals.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.rentals.entity.Address;
 
 @RepositoryRestResource(collectionResourceRel = "Addresss", path = "Addresss")
-public interface AddressRepository extends JpaRepository<Address, UUID>{
-	
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+
 }
