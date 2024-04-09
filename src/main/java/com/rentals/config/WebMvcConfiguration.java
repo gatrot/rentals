6aceca20d5d9a5c8e2ab5ca82dfabf9d83aef49c
gatrot <rotmangat@gmail.com>
2024-04-09 11:44:27 +0300

    added configs for urls and security

---
+++ src/main/java/com/rentals/config/WebMvcConfiguration.java
@@ -0,0 +1,39 @@
+package com.rentals.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * A web Configuration class for determining static resources (HTML,JS,CSS and such) location
+ * @author GatRot
+ *
+ */
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	//Array of possible static location inside the application classpath
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"};
+
+    /**
+     * Main method to configure possible locations for static resources.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+    
+    
+}
---
+++ src/main/java/com/rentals/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.rentals.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+
+/**
+ * A Web security configuration class for determining web behaviour,Authorities and user roles
+ * @author Hoffman
+ *
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	/*
+	 *Spring Dependency Injection 
+	 */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /*
+     * Creates a {@link BCryptPasswordEncoder} Spring-Bean to be 
+     * used when ever encryption  is need 
+     * @return {@link BCryptPasswordEncoder}
+     */
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*
+     *Main Web security configuration method 
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	  http
+    	     .authorizeRequests()
+    	     	 //Permit all HTTP requests that contain the word /global/ or trying to get a static resource (besides user_profile.html) 
+    	         .antMatchers("/resources/**","/index.html","/public/**", "/").permitAll() 
+    	         //Authenticate every HTTP request that trying to get to a authenticated-resorce by checking ROLE-USER
+    	         .antMatchers("/user_profile.html" , "/private/**").authenticated()
+    	         //Any request besides the above configuration : authenticate
+    	         .anyRequest().authenticated()
+    	         //Register a logout URL and a logout Success URL to be redirected to (upon successful user logout) 
+    	         .and()
+    	         .logout()
+    	         .logoutUrl("/private/logout")
+    	         .logoutSuccessUrl("/index.html") ;
+    	        
+    	         //Allow <iframe> to be open if the request came from the same origin
+    	 http
+    	 	.headers()
+	         .frameOptions()
+	         .sameOrigin()
+	         .and()
+             .csrf()
+             .disable();
+    	  
+
+    }
+
+    /*
+     * Via injection we are configuring {@link AuthenticationManagerBuilder} 
+     * to use {@link UserDetailsService} as the 
+     * and {@link BCryptPasswordEncoder} as the password encryptor. 
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    }
+    
+    
+
+    
+}
