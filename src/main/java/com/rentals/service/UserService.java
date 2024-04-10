8fc59956c11f9cb20474d8e8fface398beb0cb56
gatrot <rotmangat@gmail.com>
2024-04-10 10:45:58 +0300

    removed get all users (was a test)

--- src/main/java/com/rentals/service/UserService.java
+++ src/main/java/com/rentals/service/UserService.java
@@ -1,6 +1,5 @@
 package com.rentals.service;
 
-import java.util.List;
 import java.util.UUID;
 
 import com.rentals.entity.User;
@@ -8,12 +7,10 @@ import com.rentals.entity.User;
 public interface UserService {
 
 	Boolean createUser(User user);
-	
+
 	User findUserByEmail(String email);
-		
+
 	User getUserById(UUID userId);
-	
+
 	User confirmRegistration(User user);
-	
-	List<User> getAllUsers();
 }
